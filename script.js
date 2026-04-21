a096f230e581f96c058487333a363ba43dd271b2
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-21 09:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
 
-  startX = e.pageX; 
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
 });
 
-document.addEventListener('mousemove', (e) => {
+function handleMove(e) {
   if (!isDown) return;
 
   const walk = (e.pageX - startX) * 2;
   slider.scrollLeft = scrollLeft - walk;
-});
+}
+
+slider.addEventListener('mousemove', handleMove);
+document.addEventListener('mousemove', handleMove);
 
 document.addEventListener('mouseup', () => {
   isDown = false;
